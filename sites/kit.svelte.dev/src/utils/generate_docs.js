9252eb02393991c0aa811171c4a97fabd56e65ad
Rich Harris <richard.a.harris@gmail.com>
2021-03-15 20:47:04 -0400

    get stuff working again

--- sites/kit.svelte.dev/src/utils/generate_docs.js
+++ sites/kit.svelte.dev/src/utils/generate_docs.js
@@ -27,10 +27,10 @@ export default function generate_docs(dir) {
 	});
 
 	return fs
-		.readdirSync(`content/${dir}`)
+		.readdirSync(`content/documentation/${dir}`)
 		.filter(file => /^\d{2}-.+\.md$/.test(file))
 		.map(file => {
-			const markdown = fs.readFileSync(`content/${dir}/${file}`, 'utf-8');
+			const markdown = fs.readFileSync(`content/documentation/${dir}/${file}`, 'utf-8');
 
 			const { content, metadata } = extract_frontmatter(markdown);
 
@@ -122,6 +122,7 @@ export default function generate_docs(dir) {
 							${text}
 						</h${level}>`;
 				} else {
+					console.log({ rawtext });
 					throw new Error(`headings must be level 3 or 4`);
 				}
 			};

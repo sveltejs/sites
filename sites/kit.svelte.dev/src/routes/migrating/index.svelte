a7051ab0da352f5f9d1c55f7b17573fe44de3ffd
Conduitry <git@chor.date>
2021-03-18 23:49:43 -0400

    fix documentation edit links (#12)
    * fix documentation edit links
    
    * also fix the migrating page

--- sites/kit.svelte.dev/src/routes/migrating/index.svelte
+++ sites/kit.svelte.dev/src/routes/migrating/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
 	export async function load({ fetch }) {
-		const sections = await fetch(`migrating.json`).then(r => r.json());
+		const sections = await fetch(`migrating.json`).then((r) => r.json());
 		return {
 			props: { sections }
 		};
@@ -8,7 +8,7 @@
 </script>
 
 <script>
-	import { Docs } from '@sveltejs/site-kit'
+	import { Docs } from '@sveltejs/site-kit';
 
 	export let sections;
 </script>
@@ -16,9 +16,9 @@
 <svelte:head>
 	<title>Migration • SvelteKit</title>
 
-	<meta name="twitter:title" content="SvelteKit migration guides">
-	<meta name="twitter:description" content="The next small thing in web development">
-	<meta name="Description" content="The next small thing in web development">
+	<meta name="twitter:title" content="SvelteKit migration guides" />
+	<meta name="twitter:description" content="The next small thing in web development" />
+	<meta name="Description" content="The next small thing in web development" />
 </svelte:head>
 
-<Docs {sections} project="sveltekit" dir="migrating"/>
+<Docs {sections} project="kit" path="/documentation" dir="migrating" />

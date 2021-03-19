1304312eedd39cfcca9c1a0a56dc21a44bd4b281
Rich Harris <richard.a.harris@gmail.com>
2021-03-19 00:21:01 -0400

    use lib

--- sites/kit.svelte.dev/src/routes/docs/index.json.js
+++ sites/kit.svelte.dev/src/routes/docs/index.json.js
@@ -1,11 +1,10 @@
 import { dev } from '$app/env';
-import generate_docs from '../../utils/generate_docs.js';
+import generate_docs from '$lib/generate_docs.js';
 
 let body;
 
 export function get() {
 	if (!body || dev) {
-		console.log('>>> generating docs');
 		body = JSON.stringify(generate_docs('docs')); // TODO it errors if I send the non-stringified value
 	}
 

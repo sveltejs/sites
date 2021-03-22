da0b929ee8039ec4e769d7d144713ff473edf4ab
Rich Harris <richard.a.harris@gmail.com>
2021-03-22 17:41:19 -0400

    meaningless change to try and force a redploy

--- sites/kit.svelte.dev/src/routes/docs/index.json.js
+++ sites/kit.svelte.dev/src/routes/docs/index.json.js
@@ -4,8 +4,8 @@ import generate_docs from '$lib/generate_docs.js';
 let body;
 
 export function get() {
-	if (!body || dev) {
-		body = JSON.stringify(generate_docs('docs')); // TODO it errors if I send the non-stringified value
+	if (dev || !body) {
+		body = JSON.stringify(generate_docs('docs'));
 	}
 
 	return {

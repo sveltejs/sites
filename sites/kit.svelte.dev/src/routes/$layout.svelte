397cb82b3353b8d879fef4a00121a3e5aa40f568
Rafael Camara <rafaelcamaram@gmail.com>
2021-04-13 12:55:15 -0300

    Removing Repl from navbar

--- sites/kit.svelte.dev/src/routes/$layout.svelte
+++ sites/kit.svelte.dev/src/routes/$layout.svelte
@@ -14,9 +14,9 @@
 		<NavItem external="https://svelte.dev">Svelte</NavItem>
 	</div>
 	<div class="nav-right" slot="nav-right">
-		<NavItem external="https://svelte.dev/repl" title="Repl Page">
+		<!-- <NavItem external="https://svelte.dev/repl" title="Repl Page">
 			<img class="nav-icon" width="20px" src="./icons/repl.svg" alt="Open Svelte Repl page" />
-		</NavItem>
+		</NavItem> -->
 
 		<NavItem external="https://svelte.dev/chat" title="Discord Chat">
 			<img class="nav-icon" width="20px" src="./icons/discord.svg" alt="Open Discord chat" />

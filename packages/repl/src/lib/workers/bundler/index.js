54b71df827a7a795c427161a32e9f4616d12dadf
Rich Harris <hello@rich-harris.dev>
2021-12-15 12:52:59 -0500

    only import from local file if importer is local

--- packages/repl/src/lib/workers/bundler/index.js
+++ packages/repl/src/lib/workers/bundler/index.js
@@ -130,7 +130,7 @@ async function get_bundle(uid, mode, cache, lookup) {
 			}
 
 			// importing from another file in REPL
-			if (importee in lookup) return importee;
+			if (importee in lookup && (!importer || importer in lookup)) return importee;
 			if (importee + '.js' in lookup) return importee + '.js';
 			if (importee + '.json' in lookup) return importee + '.json';
 

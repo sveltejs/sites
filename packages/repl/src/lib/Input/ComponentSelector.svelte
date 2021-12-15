a366fff1fd502d494e079be39013eface89be376
Rich Harris <richard.a.harris@gmail.com>
2021-12-15 12:58:04 -0500

    remove scrollIntoView (#117)

--- packages/repl/src/lib/Input/ComponentSelector.svelte
+++ packages/repl/src/lib/Input/ComponentSelector.svelte
@@ -81,11 +81,6 @@
 
 		editing = component;
 
-		setTimeout(() => {
-			// TODO we can do this without IDs
-			document.getElementById(component.name).scrollIntoView(false);
-		});
-
 		components.update((components) => components.concat(component));
 		handle_select(component);
 

9d3661a74e3108039d5f0651e06f88e1f56702f4
Rich Harris <hello@rich-harris.dev>
2021-12-16 20:43:12 -0500

    highlight current section in nav

--- packages/site-kit/components/NavItem.svelte
+++ packages/site-kit/components/NavItem.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from '$app/stores';
 
-	export let segment = null;
+	export let href = null;
 	export let external = null;
 	export let title = null;
 </script>
@@ -9,8 +9,8 @@
 {#if external}
 	<li><a href={external} {title} rel="external"><slot /></a></li>
 {:else}
-	<li class:active={$page.path.split('/')[1] === segment}>
-		<a sveltekit:prefetch href={segment} {title}><slot /></a>
+	<li class:active={$page.path.startsWith(href)}>
+		<a sveltekit:prefetch {href} {title}><slot /></a>
 	</li>
 {/if}
 
@@ -22,6 +22,10 @@
 	a:hover {
 		color: var(--flash);
 		opacity: 1;
+	}
+
+	.active a {
+		color: var(--prime);
 	}
 
 	/* TODO figure out some active styles */
--- sites/kit.svelte.dev/src/routes/__layout.svelte
+++ sites/kit.svelte.dev/src/routes/__layout.svelte
@@ -2,9 +2,6 @@
 	import '@sveltejs/site-kit/base.css';
 	import { page, navigating } from '$app/stores';
 	import { Icon, Icons, Nav, NavItem, PreloadingIndicator } from '@sveltejs/site-kit';
-
-	export let segment;
-	$: segment = $page.path.split('/').pop();
 </script>
 
 <Icons />
@@ -13,26 +10,14 @@
 	<PreloadingIndicator />
 {/if}
 
-<Nav {segment} {page} logo="/images/svelte-kit-horizontal.svg">
+<Nav {page} logo="/images/svelte-kit-horizontal.svg">
 	<svelte:fragment slot="nav-center">
-		<NavItem segment="/docs">Docs</NavItem>
-		<NavItem segment="/faq">FAQ</NavItem>
-		<NavItem segment="/migrating">Migrating</NavItem>
+		<NavItem href="/docs">Docs</NavItem>
+		<NavItem href="/faq">FAQ</NavItem>
+		<NavItem href="/migrating">Migrating</NavItem>
 	</svelte:fragment>
 
 	<svelte:fragment slot="nav-right">
-		<!-- <NavItem external="https://svelte.dev/chat" title="Discord Chat">
-			<img width="20px" src="/icons/discord.svg" alt="Open Discord chat" />
-		</NavItem>
-
-		<NavItem external="https://github.com/sveltejs/kit" title="GitHub Repo">
-			<img width="20px" src="/icons/github.svg" alt="Open Svelte GitHub page" />
-		</NavItem>
-
-		<NavItem external="https://svelte.dev/" title="Svelte Homepage">
-			<img width="20px" src="./icons/svelte.svg" alt="Open Svelte page" />
-		</NavItem> -->
-
 		<NavItem external="https://svelte.dev">Svelte</NavItem>
 
 		<NavItem external="https://svelte.dev/chat" title="Discord Chat">

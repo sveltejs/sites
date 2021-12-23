47a71591a39a3e262c6d64a9fd2871b8b680666d
Geoff Rich <geoff.rich@alaskaair.com>
2021-12-23 14:24:18 -0800

    Add skip link

--- packages/site-kit/src/lib/index.js
+++ packages/site-kit/src/lib/index.js
@@ -7,3 +7,4 @@ export { default as PreloadingIndicator } from './components/PreloadingIndicator
 export { default as Nav } from './components/Nav.svelte';
 export { default as NavItem } from './components/NavItem.svelte';
 export { default as Section } from './components/Section.svelte';
+export { default as SkipLink } from './components/SkipLink.svelte';

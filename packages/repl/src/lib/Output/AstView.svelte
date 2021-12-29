1896e97b815cbf314ac9d53a2a090ace7cb38bf5
bluwy <bjornlu.dev@gmail.com>
2021-12-29 14:36:04 +0800

    fix ast flash

--- packages/repl/src/lib/Output/AstView.svelte
+++ packages/repl/src/lib/Output/AstView.svelte
@@ -6,16 +6,20 @@
 
 	const { cursor_index } = getContext('REPL');
 
-	$: path_nodes = find_deepest($cursor_index, [ast]) || [ast];
+	// $cursor_index may go over the max since ast computation is usually slower.
+	// clamping this helps prevent the collapse view flashing
+	$: max_cursor_index = !ast ? $cursor_index : Math.min($cursor_index, get_ast_max_end(ast));
 
-	function find_deepest(cursor, paths) {
+	$: path_nodes = ast ? find_deepest_path(max_cursor_index, [ast]) : [];
+
+	function find_deepest_path(cursor, paths) {
 		const value = paths[paths.length - 1];
 
 		if (!value) return;
 
 		for (const v of Object.values(value)) {
 			if (typeof v === 'object') {
-				const result = find_deepest(cursor, paths.concat([v]));
+				const result = find_deepest_path(cursor, paths.concat([v]));
 				if (result) return result;
 			}
 		}
@@ -28,6 +32,18 @@
 		) {
 			return paths;
 		}
+	}
+
+	function get_ast_max_end(ast) {
+		let max_end = 0;
+
+		for (const node of Object.values(ast)) {
+			if (node && typeof node.end === 'number' && node.end > max_end) {
+				max_end = node.end;
+			}
+		}
+
+		return max_end;
 	}
 </script>
 

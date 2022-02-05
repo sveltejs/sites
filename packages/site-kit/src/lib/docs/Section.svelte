bbdcbf4e56a79eda2bf079063d5ab457a883165e
Rich Harris <hello@rich-harris.dev>
2022-02-05 15:43:08 -0500

    ids on h2s (#293)

--- packages/site-kit/src/lib/docs/Section.svelte
+++ packages/site-kit/src/lib/docs/Section.svelte
@@ -1,17 +1,14 @@
 <script>
-	import Permalink from '../components/Permalink.svelte';
 	import Icon from '../components/Icon.svelte';
 
 	export let section;
-	export let base;
 	export let edit;
 </script>
 
 <section data-id={section.slug}>
-	<h2>
-		<span class="offset-anchor" id={section.slug} />
+	<h2 id={section.slug}>
 		{@html section.title}
-		<Permalink href="{base}#{section.slug}" />
+
 		<small>
 			<a href={edit} title="edit this section">
 				<Icon name="edit" />

593038f03437c8be50689a2adea69a4ce4c0ac68
Rich Harris <hello@rich-harris.dev>
2022-06-12 21:50:14 -0400

    make it possible to iframe svelte.dev on cross-origin-isolated sites

--- sites/svelte.dev/src/hooks.js
+++ sites/svelte.dev/src/hooks.js
@@ -10,7 +10,12 @@ export async function handle({ event, resolve }) {
 	event.locals.cookies = cookie.parse(event.request.headers.get('cookie') || '');
 	event.locals.user = await session.read(event.locals.cookies.sid);
 
-	return await resolve(event);
+	const response = await resolve(event);
+
+	response.headers.set('cross-origin-embedder-policy', 'require-corp');
+	response.headers.set('cross-origin-resource-policy', 'cross-origin');
+
+	return response;
 }
 
 /** @type {import('@sveltejs/kit').GetSession} */

3c0cc2df4beff7a5d34bab214c843e68bfabfe5a
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-08-11 09:22:14 -0700

    Update dependencies

--- sites/svelte.dev/svelte.config.js
+++ sites/svelte.dev/svelte.config.js
@@ -1,52 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
-import * as path from 'path';
-import { imagetools } from 'vite-imagetools';
-
-process.env.VITE_API_BASE = process.env.DOCS_PREVIEW
-	? 'http://localhost:8787'
-	: 'https://api.svelte.dev';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
-		adapter: adapter(),
-
-		vite: {
-			optimizeDeps: {
-				include: [
-					'codemirror',
-					'codemirror/mode/javascript/javascript.js',
-					'codemirror/mode/handlebars/handlebars.js',
-					'codemirror/mode/htmlmixed/htmlmixed.js',
-					'codemirror/mode/xml/xml.js',
-					'codemirror/mode/css/css.js',
-					'codemirror/mode/markdown/markdown.js',
-					'codemirror/addon/edit/closebrackets.js',
-					'codemirror/addon/edit/closetag.js',
-					'codemirror/addon/edit/continuelist.js',
-					'codemirror/addon/comment/comment.js',
-					'codemirror/addon/fold/foldcode.js',
-					'codemirror/addon/fold/foldgutter.js',
-					'codemirror/addon/fold/brace-fold.js',
-					'codemirror/addon/fold/xml-fold.js',
-					'codemirror/addon/fold/indent-fold.js',
-					'codemirror/addon/fold/markdown-fold.js',
-					'codemirror/addon/fold/comment-fold.js'
-				]
-			},
-			plugins: [imagetools()],
-			resolve: {
-				alias: {
-					$img: path.resolve('src/images'),
-					'@sveltejs/repl': path.resolve('../../packages/repl/src/lib'),
-					'@sveltejs/site-kit': path.resolve('../../packages/site-kit/src/lib')
-				}
-			},
-			server: {
-				fs: {
-					strict: false
-				}
-			}
-		}
+		adapter: adapter()
 	}
 };

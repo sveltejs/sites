a86bbd703f2587068aca18e4328ff14214931ed9
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-08-18 19:08:04 -0700

    don't set header twice

--- sites/svelte.dev/src/routes/examples/+layout.js
+++ sites/svelte.dev/src/routes/examples/+layout.js
@@ -1,11 +1,6 @@
 import { API_BASE } from '$lib/env';
 
-export async function load({ fetch, setHeaders }) {
+export async function load({ fetch }) {
 	const examples = await fetch(`${API_BASE}/docs/svelte/examples`).then((r) => r.json());
-
-	setHeaders({
-		'cache-control': 'public, max-age=60'
-	});
-
 	return { examples };
 }
--- sites/svelte.dev/src/routes/tutorial/+layout.js
+++ sites/svelte.dev/src/routes/tutorial/+layout.js
@@ -1,11 +1,6 @@
 import { API_BASE } from '$lib/env';
 
-export async function load({ fetch, setHeaders }) {
+export async function load({ fetch }) {
 	const tutorials = await fetch(`${API_BASE}/docs/svelte/tutorial`).then(r => r.json());
-
-	setHeaders({
-		'cache-control': 'public, max-age=60'
-	});
-
 	return { tutorials };
 }

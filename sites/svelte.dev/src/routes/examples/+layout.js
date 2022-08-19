4989e45fe2807b2bbc3a1c386b90215200245801
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-08-18 17:01:20 -0700

    fix

--- sites/svelte.dev/src/routes/examples/+layout.js
+++ sites/svelte.dev/src/routes/examples/+layout.js
@@ -4,9 +4,9 @@ import { API_BASE } from '$lib/env';
 export async function load() {
 	const examples = await fetch(`${API_BASE}/docs/svelte/examples`).then((r) => r.json());
 
-	return json(examples, {
-		cache: {
-			maxage: 60
-		}
+	setHeaders({
+		'cache-control': 'public, max-age=60'
 	});
+
+	return { examples };
 }
--- sites/svelte.dev/src/routes/tutorial/+layout.js
+++ sites/svelte.dev/src/routes/tutorial/+layout.js
@@ -4,9 +4,9 @@ import { API_BASE } from '$lib/env';
 export async function load() {
 	const tutorials = await fetch(`${API_BASE}/docs/svelte/tutorial`).then(r => r.json());
 
-	return json(tutorials, {
-		cache: {
-			maxage: 60
-		}
+	setHeaders({
+		'cache-control': 'public, max-age=60'
 	});
+
+	return { tutorials };
 }

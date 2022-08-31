13d526f718fc05bb211eeea2fc589ef06979ad91
Rich Harris <hello@rich-harris.dev>
2022-08-31 16:34:29 -0400

    quick fix

--- sites/hn.svelte.dev/src/routes/[list=category]/[page]/+page.js
+++ sites/hn.svelte.dev/src/routes/[list=category]/[page]/+page.js
@@ -3,7 +3,7 @@ export async function load({ params, fetch }) {
 	const list = params.list === 'top' ? 'news' : params.list === 'new' ? 'newest' : params.list;
 
 	return {
-		list,
+		list: params.list,
 		page: +params.page,
 		items: await fetch(`https://api.hnpwa.com/v0/${list}/${params.page}.json`).then((r) => r.json())
 	};

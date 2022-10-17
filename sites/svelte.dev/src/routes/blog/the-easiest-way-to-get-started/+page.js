9e828391c5240943332fb11b23b3f41bb0edc5c4
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-10-17 15:20:37 -0700

    fix redirect

--- sites/svelte.dev/src/routes/blog/the-easiest-way-to-get-started/+page.js
+++ sites/svelte.dev/src/routes/blog/the-easiest-way-to-get-started/+page.js
@@ -1,5 +1,6 @@
 import { redirect } from '@sveltejs/kit';
+import { dev } from '$app/environment';
 
-export function load({ event }) {
-  throw redirect(308, `${event.url.origin}/docs/introduction`);
+export function load() {
+  throw redirect(dev ? 307 : 308, '/docs');
 }

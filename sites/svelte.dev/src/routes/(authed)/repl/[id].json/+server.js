5101944562cbddfef264ccaf95828e0ab14aac4c
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-11-21 14:27:42 -0800

    Update sites/svelte.dev/src/routes/(authed)/repl/[id].json/+server.js
    Co-authored-by: Conduitry <git@chor.date>

--- sites/svelte.dev/src/routes/(authed)/repl/[id].json/+server.js
+++ sites/svelte.dev/src/routes/(authed)/repl/[id].json/+server.js
@@ -6,7 +6,7 @@ import * as gist from '$lib/db/gist';
 import { API_BASE } from '$lib/env';
 
 const UUID_REGEX =
-	/^([a-f]|[0-9]){8}-?([a-f]|[0-9]){4}-?([a-f]|[0-9]){4}-?([a-f]|[0-9]){4}-?([a-f]|[0-9]){12}$/;
+	/^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f{4}-?[0-9a-f]{12}$/;
 
 /** @type {Set<string>} */
 let examples;

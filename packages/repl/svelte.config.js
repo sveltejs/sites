c87bacbb43b2eb0b733ca97ad76fbb135dc84516
Puru Vijay <devpuruvj@gmail.com>
2023-07-10 02:09:18 +0530

    Push

--- packages/repl/svelte.config.js
+++ packages/repl/svelte.config.js
@@ -1,8 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({})
 	}
 };

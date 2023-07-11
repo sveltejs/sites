c64c4166bb926c635523305b8fbce76ada3f12d8
Puru Vijay <devpuruvj@gmail.com>
2023-07-12 02:02:53 +0530

    Lightningcss, 500ms faster builds than before

--- sites/hn.svelte.dev/vite.config.js
+++ sites/hn.svelte.dev/vite.config.js
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			targets: browserslistToTargets(browserslist(['defaults', 'not dead']))
+		}
+	},
+	build: {
+		cssMinify: 'lightningcss'
+	}
 };
 
 export default config;

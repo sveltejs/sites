37fb3d65f04aea16867be738f698a6acdd951e22
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-07-17 21:27:31 -0700

    comment

--- sites/hn.svelte.dev/svelte.config.js
+++ sites/hn.svelte.dev/svelte.config.js
@@ -3,6 +3,8 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		// new projects use adapter-auto by default
+		// we use adapter-vercel here to use more efficient edge serving
 		adapter: adapter({ runtime: 'edge' }),
 
 		inlineStyleThreshold: 5000

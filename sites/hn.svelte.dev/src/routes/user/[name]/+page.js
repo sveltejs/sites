a2741678279aa46638c02a57715f96eddacc0629
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2023-12-15 12:16:25 +0530

    Push (#550)

--- sites/hn.svelte.dev/src/routes/user/[name]/+page.js
+++ sites/hn.svelte.dev/src/routes/user/[name]/+page.js
@@ -1,5 +1,5 @@
 export const csr = false;
 
 export async function load({ params, fetch }) {
-	return fetch(`https://api.hnpwa.com/v0/user/${params.name}.json`).then((r) => r.json());
+	return await fetch(`https://api.hnpwa.com/v0/user/${params.name}.json`).then((r) => r.json());
 }

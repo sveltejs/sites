7cfa06af2ed08d1c5d79b3482a479f583bb9b349
GrygrFlzr <git@cybeast.dev>
2026-06-26 11:03:48 +0700

    merge svelte config into vite config

--- sites/hn.svelte.dev/vite.config.ts
+++ sites/hn.svelte.dev/vite.config.ts
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit({
+			adapter: adapter({
+				isr: {
+					allowQuery: [], // we don't use any query params
+					expiration: 60
+				}
+			}),
+			inlineStyleThreshold: 5000,
+			compilerOptions: {
+				runes: true
+			}
+		})
+	]
 });
